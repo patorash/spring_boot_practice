8da443d73ca08ca273ff6fcc8abc9720fb517260
patorash <chariderpato@gmail.com>
2015-03-03 23:35:12 +0900

    Spring Data JPAを使ってみた。

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -1,5 +1,7 @@
 package sample;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,7 +9,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    CustomerRepository repository;
+
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
@@ -17,6 +23,38 @@ public class Application {
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
+        }
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Customer("Jack", "Bauer"));
+        repository.save(new Customer("Chloe", "O'Brian"));
+        repository.save(new Customer("Kim", "Bauer"));
+        repository.save(new Customer("David", "Palmer"));
+        repository.save(new Customer("Michelle", "Dessler"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer by ID
+        Customer customer = repository.findOne(1L);
+        System.out.println("Customer found with findOne(1L):");
+        System.out.println("--------------------------------");
+        System.out.println(customer);
+        System.out.println();
+
+        // fetch customers by last name
+        System.out.println("Customer found with findByLastName('Bauer'):");
+        System.out.println("--------------------------------------------");
+        for (Customer bauer : repository.findByLastName("Bauer")) {
+            System.out.println(bauer);
         }
     }
 }
